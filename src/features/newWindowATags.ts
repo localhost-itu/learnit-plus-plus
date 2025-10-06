5747ad7b3f793bddf0b51e922262454f826fb2d3
Patrick Matthiesen <43612965+PatrickMatthiesen@users.noreply.github.com>
2025-10-06 23:12:02 +0200

    update target blank styling

--- src/features/newWindowATags.ts
+++ src/features/newWindowATags.ts
@@ -1,10 +1,9 @@
 export function replaceResourceTags(){
-    const resourceLinks = document.querySelectorAll(".resource .activityname a.aalink");
+    const resourceLinks = document.querySelectorAll("a.aalink.stretched-link[onclick]");
     if(resourceLinks.length == 0) return;
     resourceLinks.forEach((item: Element) => {
         item.setAttribute("target", "_blank");
         item.removeAttribute("onclick");
         item.setAttribute("href", item.getAttribute("href") + "&redirect=1");
     });
-
 }
